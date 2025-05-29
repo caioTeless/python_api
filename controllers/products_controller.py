53008bd030de073259983a0ecf09f98518f4ea98
caioTeless <caioo.teless@gmail.com>
2025-05-29 19:40:09 -0300

    fix in controllers

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -3,18 +3,3 @@ from controllers.base_controller import BaseController
 class ProductsController:
     def __init__(self):
         self.base = BaseController('products')
-        
-    def add(self):
-        return self.base.add()
-
-    def get_all(self):
-        return self.base.get_all()
-
-    def get(self, id):
-        return self.base.get(id)
-
-    def update(self, id):
-        return self.base.update(id)
-
-    def delete(self, id):
-        return self.base.delete(id)
